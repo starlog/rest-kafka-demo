5880b42e68030c6915118a60f5cf100b4480918f
Felix <felix@washswat.com>
2023-06-14 12:29:52 +0900

    Added more description

--- server1/service/DemoService.ts
+++ server1/service/DemoService.ts
@@ -27,25 +27,36 @@ export async function init() {
   try {
     console.log('init start.');
 
+    //----------------------------------------------------------------------------------------------
+    // Kafka Client 생성
+    // ----------------------------------------------------------------------------------------------
     const kafka = new Kafka({
       clientId: KAFKA_CLIENT_ID,
       brokers: kafkaBroker,
     });
 
+    //----------------------------------------------------------------------------------------------
+    // Kafka Producer 생성
+    //----------------------------------------------------------------------------------------------
     const kafkaProd = new Kafka({
       clientId: KAFKA_CLIENT_ID_PROD,
       brokers: kafkaBroker,
     });
 
+    //----------------------------------------------------------------------------------------------
     // Consumer 생성
+    //----------------------------------------------------------------------------------------------
     consumer = kafka.consumer({ groupId: KAFKA_GROUP_ID });
     await consumer.connect();
     await consumer.subscribe({ topic: 'felix_demo', fromBeginning: true });
 
+    //----------------------------------------------------------------------------------------------
     // Producer 생성
+    //----------------------------------------------------------------------------------------------
     producer = kafkaProd.producer({ createPartitioner: Partitioners.LegacyPartitioner });
     await producer.connect();
 
+    //----------------------------------------------------------------------------------------------
     // Consumer 구동
     //----------------------------------------------------------------------------------------------
     await consumer.run({
@@ -54,11 +65,16 @@ export async function init() {
         partition,
         message,
       }) => {
+        //------------------------------------------------------------------------------------------
         // 원하는 메시지 (key가 KAFKA_MESSAGE_KEY_RECEIVE)인 경우에만 처리)
+        //------------------------------------------------------------------------------------------
         if (message.key.toString() === KAFKA_MESSAGE_KEY_RECEIVE) {
           console.log(`[KafkaConsumer] topic:${topic}, partition:${partition}, offset:${message.offset}`);
           const dataObject = JSON.parse(message.value.toString());
           console.log(`[KafkaConsumer] Saving to message queue:${dataObject?.uuid}`);
+          //----------------------------------------------------------------------------------------
+          // TTL 시간까지만 저장해서 필요없는 메시지를 자동으로 삭제한다. (데모를 위해서 짧게 저장함.)
+          //----------------------------------------------------------------------------------------
           await messageQueue.push(dataObject?.uuid, dataObject, TTL_SECONDS);
         }
       },
@@ -75,12 +91,19 @@ export async function init() {
 //--------------------------------------------------------------------------------------------------
 async function sendSingleMessageToKafka(uuid:string) {
   try {
+    //----------------------------------------------------------------------------------------------
+    // uuid 필드에 요청 메시지를 구분할 수 있는 값을 넣는다.
+    //----------------------------------------------------------------------------------------------
     const dataObject = {
       uuid,
       type: 'myType',
       description: 'description1',
     };
 
+    //----------------------------------------------------------------------------------------------
+    // key 필드에 전송 목적지를 구별할 수 있는 값을 넣는다. 전송/수신을 동일한 토픽을 사용하기 때문이다.
+    // 만약 전송/수신을 다른 토픽을 사용한다면 key 필드를 이용한 구별은 필요하지 않다.
+    //----------------------------------------------------------------------------------------------
     const message = {
       key: KAFKA_MESSAGE_KEY_SEND,
       value: JSON.stringify(dataObject),
@@ -111,18 +134,25 @@ export async function demoRequest() {
     const examples = {
       result: 'result',
     };
+    //----------------------------------------------------------------------------------------------
     // 요청을 메시지로 전송한다.
+    //----------------------------------------------------------------------------------------------
     const uuid = uuidv4();
     const sendResult:boolean = await sendSingleMessageToKafka(uuid);
     console.log(`sendResult:${sendResult}`);
 
-    let resultMessage = null;
+    //----------------------------------------------------------------------------------------------
     // 원하는 메시지가 도착할때 까지 기다린다.
+    //----------------------------------------------------------------------------------------------
+    let resultMessage = null;
     while (!resultMessage) {
       resultMessage = await messageQueue.get(uuid);
       await new Promise((resolve) => setTimeout(resolve, 70));
     }
 
+    //----------------------------------------------------------------------------------------------
+    // 응답을 REST 로 return 한다.
+    //----------------------------------------------------------------------------------------------
     console.log(`messageQueue length = ${messageQueue.length()}`);
     examples.result = resultMessage;
     return examples;
--- server2/index.ts
+++ server2/index.ts
@@ -24,22 +24,32 @@ async function init() {
   try {
     console.log('init start.');
 
+    //----------------------------------------------------------------------------------------------
+    // Kafka Client 생성
+    // ----------------------------------------------------------------------------------------------
     const kafka = new Kafka({
       clientId: KAFKA_CLIENT_ID,
       brokers: kafkaBroker,
     });
 
+    //----------------------------------------------------------------------------------------------
+    // Kafka Producer 생성
+    //----------------------------------------------------------------------------------------------
     const kafkaProd = new Kafka({
       clientId: KAFKA_CLIENT_ID_PROD,
       brokers: kafkaBroker,
     });
 
+    //----------------------------------------------------------------------------------------------
     // Consumer 생성
+    //----------------------------------------------------------------------------------------------
     consumer = kafka.consumer({ groupId: KAFKA_GROUP_ID });
     await consumer.connect();
     await consumer.subscribe({ topic: 'felix_demo', fromBeginning: true });
 
+    //----------------------------------------------------------------------------------------------
     // Producer 생성
+    //----------------------------------------------------------------------------------------------
     producer = kafkaProd.producer({ createPartitioner: Partitioners.LegacyPartitioner });
     await producer.connect();
 
@@ -52,6 +62,10 @@ async function init() {
 //--------------------------------------------------------------------------------------------------
 async function sendSingleMessageToKafka(uuid:string) {
   try {
+    //----------------------------------------------------------------------------------------------
+    // uuid 필드에 요청 메시지를 구분할 수 있는 값을 넣는다.
+    // extraData 필드를 생성해서, 서버에서 처리한 결과를 시뮬레이션 한다.
+    //----------------------------------------------------------------------------------------------
     const dataObject = {
       uuid,
       type: 'myType',
@@ -59,6 +73,10 @@ async function sendSingleMessageToKafka(uuid:string) {
       extraData: randomUUID(), // 처리한 결과 데이터를 시뮬레이션
     };
 
+    //----------------------------------------------------------------------------------------------
+    // key 필드에 전송 목적지를 구별할 수 있는 값을 넣는다. 전송/수신을 동일한 토픽을 사용하기 때문이다.
+    // 만약 전송/수신을 다른 토픽을 사용한다면 key 필드를 이용한 구별은 필요하지 않다.
+    //----------------------------------------------------------------------------------------------
     const message = {
       key: KAFKA_MESSAGE_KEY_SEND,
       value: JSON.stringify(dataObject),
@@ -77,6 +95,8 @@ async function sendSingleMessageToKafka(uuid:string) {
 }
 
 //--------------------------------------------------------------------------------------------------
+// Kafka consumer server start
+//--------------------------------------------------------------------------------------------------
 async function run() {
   await consumer.run({
     eachMessage: async ({
@@ -84,12 +104,16 @@ async function run() {
       partition,
       message,
     }) => {
+      //--------------------------------------------------------------------------------------------
       // 원하는 오브잭트만 처리
+      //--------------------------------------------------------------------------------------------
       if (message.key.toString() === KAFKA_MESSAGE_KEY_RECEIVE) {
         console.log(`topic:${topic}, partition:${partition}, offset:${message.offset}`);
         const dataObject = JSON.parse(message.value.toString());
         console.log(`received dataObject:${JSON.stringify(dataObject)}`);
+        //------------------------------------------------------------------------------------------
         // 처리하는데 시간이 걸리는것을 시뮬레이션
+        //------------------------------------------------------------------------------------------
         await new Promise((resolve) => setTimeout(resolve, 20));
         console.log(`sending dataObject.uuid:${dataObject.uuid}`);
         await sendSingleMessageToKafka(dataObject.uuid);
